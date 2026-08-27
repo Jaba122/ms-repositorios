89c87bcb79721f28a087e364ecf24295e26f3510
Jaba122 <ja.nunezo@duocuc.cl>
2026-08-27 15:02:55 -0400

    inicio del microservicio

---
+++ src/main/java/com/duocconecta/ms_repositorios/MsRepositoriosApplication.java
@@ -0,0 +1,13 @@
+package com.duocconecta.ms_repositorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRepositoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsRepositoriosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duocconecta/ms_repositorios/MsRepositoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duocconecta.ms_repositorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRepositoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
